14cc948eac139b6d72324fb0bb860ad224e3a1ba
n00nietzsche <n00nietzsche@gmail.com>
2021-08-31 22:03:34 +0900

    스프링 MVC 2편 - 예외처리와 오류페이지 #6 스프링 부트 - 오류 페이지 1

--- src/main/java/hello/exception/servlet/ServletExController.java
+++ src/main/java/hello/exception/servlet/ServletExController.java
@@ -20,6 +20,11 @@ public class ServletExController {
         response.sendError(404, "404 에러!");
     }
 
+    @GetMapping("/error-400")
+    public void error400(HttpServletResponse response) throws IOException {
+        response.sendError(400, "400 에러!");
+    }
+
     @GetMapping("/error-500")
     public void error500(HttpServletResponse response) throws IOException {
         response.sendError(500);
